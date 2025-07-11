c68d1f4a9c075fdded928eb362fdbfcd6f14baaf
Sameer <sameerk0601@gmail.com>
2025-07-11 17:31:22 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,4 @@
-AWS_SECRET = "AKIAxxxxxxxxxxxxxxxxxx"
-PASSWORD = "MyDummyPassword123!"
+# test.py
+AWS_SECRET_ACCESS_KEY = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+API_KEY = "sk_live_51Nxxxxxx"  # Stripe secret
+PRIVATE_KEY = "-----BEGIN RSA PRIVATE KEY-----"
